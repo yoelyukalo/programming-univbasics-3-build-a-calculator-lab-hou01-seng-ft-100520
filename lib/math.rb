e15fa93a9adec47be47c688e07ccdd69666a73a4
Yoel Yukalo <yoelyukalo@gmail.com>
2020-09-13 18:54:42 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,21 +4,23 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  subsum = num1 - num2
+  subsum
 end
 
 def division(num1, num2)
-
+  divsum = num2 / num1
 end
 
 def multiplication(num1, num2)
-
+  mulsum = num1 * num2
+  mulsum
 end
 
 def modulo(num1, num2)
-
+  modsum = num2 % num1
 end
 
 def square_root(num)
-
+  
 end
